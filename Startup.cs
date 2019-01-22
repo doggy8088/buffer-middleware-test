c4b0b1dcc6f7568a79a638e9879d92f67b911cc9
Will <doggy.huang@gmail.com>
2019-01-22 23:45:41 +0800

    完成 Response Buffering 開發

--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -17,22 +18,44 @@ namespace buffer_middleware
         {
 
         }
+        private Stream _original;
+        private MemoryStream _buffered;
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            // dotnet add package Microsoft.AspNetCore.Buffering
-            app.UseResponseBuffering();
+            app.Use(async(context, next) =>
+            {
+                if (context.Response.HasStarted)
+                {
+                    throw new Exception("Cannot buffer response, it has already started");
+                }
 
-            app.Use(async (context, next) => {
+                _original = context.Response.Body;
+                _buffered = new MemoryStream();
+
+                context.Response.Body = _buffered;
 
                 await next();
 
-                // TODO: I'd like to get response from middleware below. ( app.Run() )
-                context.Response.Body.ReadByte();
+                if (_buffered != null)
+                {
+                    _buffered.Seek(0, SeekOrigin.Begin);
+                    context.Response.Body = _original;
+
+                    using(var s = new StreamReader(_buffered))
+                    using(var w = new StreamWriter(context.Response.Body))
+                    {
+                        var str = s.ReadToEnd().Replace("World", "Will");
+                        await w.WriteLineAsync(str);
+                    }
+
+                    _buffered.Dispose();
+                    _buffered = null;
+                }
             });
 
-            app.Run(async (context) =>
+            app.Run(async(context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
             });
